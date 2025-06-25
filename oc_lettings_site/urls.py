d7991f8ee47b31b3e0f8dd0e2fc43c93c4adfc7b
Trissi <Rayatrissi1@gmail.com>
2025-06-25 16:37:27 +0200

    Feat: Add custom 404 and 500 error pages
    Created custom templates for 404 (Not Found) and 500 (Server Error) responses. Configured URLconf and views to handle these errors gracefully.

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from . import views
+
+
+handler404 = 'oc_lettings_site.views.handler404'
+handler500 = 'oc_lettings_site.views.handler500'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lettings/', include('lettings.urls', namespace='lettings')),
-    path('profiles/', include('profiles.urls', namespace='profiles')),
+    path('lettings/', include('lettings.urls')),
+    path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
 ]
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from lettings.models import Letting
+from profiles.models import Profile
 
 
 def index(request):
     """
     Renders and displays the main index page of the website.
     """
-    return render(request, 'index.html')
+    lettings_list = Letting.objects.all()
+    profiles_list = Profile.objects.all()
+
+    context = {'lettings_list': lettings_list, 'profiles_list': profiles_list}
+    return render(request, 'index.html', context)
+
+
+def handler404(request, exception):
+    """
+    Custom view for 404 Page Not Found errors.
+    """
+    return render(request, '404.html', status=404)
+
+
+def handler500(request):
+    """
+    Custom view for 500 Internal Server Error.
+    """
+    return render(request, '500.html', status=500)
