692e36d6ce0e60123596bc2f8552f4dcb74089da
Trissi <Rayatrissi1@gmail.com>
2025-06-27 17:27:16 +0200

    docs: Configure Sphinx for Django project

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,16 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+import django
+
+# Add the project root to the Python path
+sys.path.insert(0, os.path.abspath('../..'))
+
+# Set up the Django environment
+os.environ['DJANGO_SETTINGS_MODULE'] = 'oc_lettings_site.settings'
+django.setup()
 
 project = 'Orange County Lettings'
 copyright = '2025, Saleh Trissi'
@@ -14,15 +24,16 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
